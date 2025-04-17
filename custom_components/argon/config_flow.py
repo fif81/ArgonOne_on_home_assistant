ef4f51839345de9ca4a9612c97c85a71076da388
fif81 <Franz.FD.Fischer@web.de>
2025-04-17 13:03:30 +0000

    Made device option translations work

--- custom_components/argon/config_flow.py
+++ custom_components/argon/config_flow.py
@@ -5,6 +5,11 @@ import logging
 import voluptuous as vol
 
 from homeassistant import config_entries
+from homeassistant.helpers.selector import (
+    SelectSelector,
+    SelectSelectorConfig,
+    SelectSelectorMode,
+)
 
 from .const import DOMAIN, DeviceModel
 
@@ -12,10 +17,9 @@ _LOGGER = logging.getLogger(__name__)
 
 
 class ArgonOneConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for ArgonOne."""
+    """Handle a config flow for Argon."""
 
     VERSION = 1
-    SM_BUS_OPTION = [0, 1]
 
     def __init__(self) -> None:
         """Initialize the config flow."""
@@ -23,18 +27,30 @@ class ArgonOneConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None) -> config_entries.ConfigFlowResult:
         """Handle the device step."""
+        errors = {}
         if user_input is not None:
             self.device_model = user_input.get("device_model")
-            return self.async_create_entry(
-                title=self.device_model,
-                data={"device_model": DeviceModel.__getitem__(self.device_model)},
-            )
+            try:
+                return self.async_create_entry(
+                    title=self.device_model,
+                    data={
+                        "device_model": DeviceModel.__getitem__(self.device_model).name
+                    },
+                )
+            except:
+                errors["base"] = "something_went_wrong"
 
         options = {
-            vol.Required(
-                "device_model",
-                default=DeviceModel.ARGON_ONE_V2,
-            ): vol.In([e.name for e in DeviceModel]),
+            vol.Required("device_model"): SelectSelector(
+                SelectSelectorConfig(
+                    options=[e.name for e in DeviceModel],
+                    sort=True,
+                    mode=SelectSelectorMode.DROPDOWN,
+                    translation_key="device_model_options",
+                )
+            ),
         }
 
-        return self.async_show_form(step_id="user", data_schema=vol.Schema(options))
+        return self.async_show_form(
+            step_id="user", data_schema=vol.Schema(options), errors=errors
+        )
